9ad22e1973f7d51344ff3c6ab08649a45b49f24c
cerebrate <avatar@arkane-systems.net>
2015-02-09 18:32:31 -0600

    Version 1.0

--- carob/Program.cs
+++ carob/Program.cs
@@ -1,15 +1,136 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region header
 
-namespace carob
+// carob - Program.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2015.  All rights reserved.
+// 
+// Created: 2015-02-09 5:09 PM
+
+#endregion
+
+#region using
+
+using System ;
+using System.Diagnostics ;
+using System.Linq ;
+using System.Security.Principal ;
+
+#endregion
+
+namespace ArkaneSystems.Carob
 {
-    class Program
+    public static class Program
     {
-        static void Main (string[] args)
+        private static string chocolateyLocation ;
+
+        private static int Main (string[] args)
         {
+            // Get and parse the command line.
+            var pargs = Environment.CommandLine ;
+            if (pargs[0] == '"')
+            {
+                // Cut one character beyond second doublequote.
+                pargs = pargs.Substring (pargs.IndexOf ('"', 1) + 1) ;
+            }
+            else
+            {
+                // Cut at first space, if it exists; if not, return empty string.
+                var i = pargs.IndexOf (' ') ;
+                pargs = i >= 0 ? pargs.Substring (i) : string.Empty ;
+            }
+
+            // Then, find chocolatey.
+            chocolateyLocation = FindChocolatey () ;
+
+            // Then, check if we have administrative rights.
+            var identity = WindowsIdentity.GetCurrent () ;
+            var principal = new WindowsPrincipal (identity) ;
+
+            var areAdmin = principal.IsInRole (WindowsBuiltInRole.Administrator) ;
+
+            // Then, check if this is a self-invoke.
+            var parent = ParentProcessUtilities.GetParentProcess () ;
+            if (parent.ProcessName != @"carob")
+            {
+                if (!areAdmin)
+                {
+                    // User is NOT an administrator.
+                    Console.WriteLine ("carob: administrative rights not present. checking further.") ;
+
+                    // Is user in Administrators group? (i.e., has installation rights.)
+                    if (principal.Claims.Any (c => c.Value == "S-1-5-32-544"))
+                    {
+                        Console.WriteLine ("carob: elevation to admin possible. invoking wrapper.") ;
+
+                        // Self-invoke as a wrapper.
+                        var psi = new ProcessStartInfo () ;
+                        psi.FileName = Process.GetCurrentProcess ().MainModule.FileName ;
+                        psi.Arguments = pargs ;
+
+                        // Force UAC invoke.
+                        psi.Verb = "runas" ;
+
+                        // Run wrapper and wait for it to exit.
+                        var selfprocess = Process.Start (psi) ;
+                        selfprocess.WaitForExit () ;
+                        return selfprocess.ExitCode ;
+                    }
+                    Console.WriteLine ("carob: this user cannot elevate to admin. fail.") ;
+                    return 1 ;
+                }
+                // User is already an administrator. Pass straight through to chocolatey.
+                Console.WriteLine ("carob: administrative rights present. passthrough.") ;
+                var chocprocess = InvokeChocolatey (pargs) ;
+
+                return chocprocess.ExitCode ;
+            }
+            // We are a self-invoke (as admin).
+            if (!areAdmin)
+                throw new InvalidOperationException ("carob: not admin on self-invoke. fatal error. epic fail.") ;
+
+            // Invoke chocolatey.
+            var process = InvokeChocolatey (pargs) ;
+
+            // Wait to be dismissed.
+            Console.WriteLine ("\ncarob: waiting for user. review chocolatey output and press any key.") ;
+
+            // Clear the buffer.
+            while (Console.KeyAvailable)
+                Console.ReadKey () ;
+
+            Console.ReadKey (true) ;
+
+            return process.ExitCode ;
+        }
+
+        private static Process InvokeChocolatey (string pargs)
+        {
+            var psi = new ProcessStartInfo () ;
+            psi.FileName = chocolateyLocation ;
+            psi.Arguments = pargs ;
+
+            // Without shell-execute, this will launch in the same command-line window.
+            psi.UseShellExecute = false ;
+
+            // Run chocolatey and wait for it to exit.
+            var process = Process.Start (psi) ;
+            process.WaitForExit () ;
+            return process ;
+        }
+
+        public static string FindChocolatey ()
+        {
+            var retval = Environment.ExpandEnvironmentVariables (@"%ChocolateyInstall%\choco.exe") ;
+            if (retval.Length <= 10)
+            {
+                Console.WriteLine ("carob: chocolatey not found, aborting.") ;
+                Environment.Exit (130) ;
+            }
+
+            return retval ;
         }
     }
 }
--- carob/Properties/AssemblyInfo.cs
+++ carob/Properties/AssemblyInfo.cs
@@ -1,26 +1,50 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿#region header
+
+// carob - AssemblyInfo.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2015.  All rights reserved.
+// 
+// Created: 2015-02-09 5:09 PM
+
+#endregion
+
+#region using
+
+using System.Reflection ;
+using System.Runtime.InteropServices ;
+
+#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("carob")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("carob")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyTitle ("carob")]
+[assembly: AssemblyDescription ("An admin-promotion wrapper for Chocolatey.")]
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration ("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany ("Arkane Systems")]
+[assembly: AssemblyProduct ("carob")]
+[assembly: AssemblyCopyright ("Copyright © Arkane Systems 2015")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+
+[assembly: ComVisible (false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("acadb46b-a95b-4265-a1ce-657c23f4b7c8")]
+
+[assembly: Guid ("acadb46b-a95b-4265-a1ce-657c23f4b7c8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +56,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
